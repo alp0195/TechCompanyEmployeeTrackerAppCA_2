025b6c49da3d45c70b534488541e543902ad0475
alp0195 <alperenzyl@gmail.com>
2025-04-24 23:17:48 +0300

    do while block added for options menu

---
+++ src/techcompanyemployeetrackerappca_2/TechCompanyEmployeeTrackerAppCA_2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package techcompanyemployeetrackerappca_2;
+import java.util.Scanner;
+/**
+ *
+ * @author alper
+ */
+public class TechCompanyEmployeeTrackerAppCA_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MenuOptionsCA_2 selected = null;
+        
+        do{ //menu will run until user choose option 5
+            MenuOptionsCA_2.printMenu();
+            System.out.println("Please enter a number to select an option.");
+            int userInput = scanner.nextInt();        
+        
+            try{            
+                selected = MenuOptionsCA_2.fromInt(userInput);
+                System.out.println(selected.getTopmenu());
+            
+            }catch(IllegalArgumentException e){
+                System.out.println("Invalid option has chosen.");
+            }
+        }while(selected != MenuOptionsCA_2.EXIT);
+        
+    }
+    
+}
