d4318c60e2713b577fc495392432c0a71791b346
alp0195 <alperenzyl@gmail.com>
2025-04-26 13:07:00 +0300

    Add employee and generate random employee methods will develop in this class.

---
+++ src/EmployeeAddingMethodsCA_2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author alper
+ */
+public class EmployeeAddingMethodsCA_2 {
+    
+    //Title and Departmant options
+    private static String[] TITLES = {"Head Manager", "Assistant Manager", "Team Lead"};
+    private static String[] DEPARTMANTS = {"Customer Service", "Technical Support", "HR"};
+    
+    public static void addEmpoyee(String EmployeeList.txt, Scanner scanner){
+    
+    }
+    
+    public static void generateRandom(String EmployeeList.txt){
+    
+    }
+    
+    
+}
